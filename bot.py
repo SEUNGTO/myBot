8ce61c1c9801732c2fa0fa339295b855a57ed08e
SEUNGTO <tmdduq9008@gmail.com>
2023-10-01 16:55:49 +0900

    git commit -m '231001' git push origin master

--- bot.py
+++ bot.py
@@ -1,39 +1,35 @@
 import requests
-import os
+from flask import Flask, request
 
-TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
-THEMES_URL = os.getenv('THEMES_JSON_URL')
+app = Flask(__name__)
 
-offset = None
-
-# git test
-# git test1
+TOKEN = 'YOUR_BOT_TOKEN'
+URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+THEMES_URL = 'https://raw.githubusercontent.com/SEUNGTO/botdata/main/themes.json'
 
 def get_themes_from_github(stock_code):
-
     response = requests.get(THEMES_URL)
     themes_data = response.json()
     return themes_data.get(stock_code, [])
 
-while True:
-    params = {'offset': offset, 'timeout': 30}
-    response = requests.get(URL, params=params)
-    messages = response.json().get('result', [])
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    update = request.json
+    chat_id = update['message']['chat']['id']
+    text = update['message']['text']
 
-    for message in messages:
-        offset = message['update_id'] + 1
-        chat_id = message['message']['chat']['id']
-        text = message['message']['text']
+    if text.startswith('/theme'):
+        command, stock_code = text.split(' ', 1)
+        themes = get_themes_from_github(stock_code)
 
-        if text.startswith('/theme'):
-            command, stock_code = text.split(' ', 1)
-            themes = get_themes_from_github(stock_code)
+        if themes:
+            theme_message = f"The themes for {stock_code} are: {', '.join(themes)}"
+        else:
+            theme_message = f"No themes found for {stock_code}"
 
-            if themes:
-                theme_message = f"The themes for {stock_code} are: {', '.join(themes)}"
-            else:
-                theme_message = f"No themes found for {stock_code}"
+        requests.post(URL, json={'chat_id': chat_id, 'text': theme_message})
 
-            requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage',
-                          json={'chat_id': chat_id, 'text': theme_message})
+    return 'OK'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
