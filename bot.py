7e40f8c33cc2f6dab9995efff523d1e778011f78
SEUNGTO <tmdduq9008@gmail.com>
2023-10-01 17:05:08 +0900

    git commit -m '231001' git push origin master

--- bot.py
+++ bot.py
@@ -1,35 +1,33 @@
+import json
 import requests
-from flask import Flask, request
-
-app = Flask(__name__)
 
 TOKEN = 'YOUR_BOT_TOKEN'
 URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 THEMES_URL = 'https://raw.githubusercontent.com/SEUNGTO/botdata/main/themes.json'
 
-def get_themes_from_github(stock_code):
+def send_message(chat_id, text):
+    params = {
+        "chat_id": chat_id,
+        "text": text
+    }
+    requests.post(URL, params=params)
+
+
+def get_theme(stock_code):
     response = requests.get(THEMES_URL)
     themes_data = response.json()
     return themes_data.get(stock_code, [])
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    update = request.json
-    chat_id = update['message']['chat']['id']
-    text = update['message']['text']
+def main():
+    update = json.loads(input())
+    chat_id = update["message"]["chat"]["id"]
+    message_text = update["message"]["text"]
 
-    if text.startswith('/theme'):
-        command, stock_code = text.split(' ', 1)
-        themes = get_themes_from_github(stock_code)
+    if message_text.startswith("/theme"):
+        stock_code = message_text.split()[-1]
+        theme = get_theme(stock_code)
+        send_message(chat_id, f"{stock_code} 종목의 테마는 '{theme}'입니다.")
 
-        if themes:
-            theme_message = f"The themes for {stock_code} are: {', '.join(themes)}"
-        else:
-            theme_message = f"No themes found for {stock_code}"
-
-        requests.post(URL, json={'chat_id': chat_id, 'text': theme_message})
-
-    return 'OK'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    main()
